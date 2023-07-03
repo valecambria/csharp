493157108afc9749ae6b5bd0e217a9de02ff789e
vale <vale.cambria@hotmail.com>
2023-07-03 00:02:38 -0300

    agregando practica en c#

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre: ");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Hello, " + nombre + " !");
+            if(nombre == "valentino"){
+                Console.WriteLine("Introduce tu edad");
+                string edad = Console.ReadLine();
+                Console.WriteLine(nombre + " tenes " + edad + " años!");
+            }
+            string date = DateTime.Now.ToString("dd-MM-yyyy");
+            Console.WriteLine(date);
+        }
+    }
+}
